c979031892daf2afb3cff58218e16b738fe57f12
unknown <apoints96@gmail.com>
2021-11-03 15:03:53 +0300

    added new applications

--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'author',
+    'book',
+    'homepage',
+    'order',
 ]
 
 MIDDLEWARE = [
