001fb3aef81bdcfc04d6944ca8810a46375a1a15
unknown <apoints96@gmail.com>
2021-12-11 17:48:12 +0300

    for deploy

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,10 +5,11 @@ from .views import (loginuser,
 
 
 urlpatterns = [
-    # Creating a user on the site. Implementation of the form through the model on the fly.
-    # Add import CustomUserFormView.
-    # path('form/', CustomUserFormView.as_view(), name='customuser_form'),
-    path('login/', loginuser, name='loginuser'),
-    path('logout/', logoutuser, name='logoutuser'),
-    path('signup/', signupuser, name='signupuser'),
+    ## Creating a user on the site. Implementation of the form through the model on the fly.
+    ## Add import CustomUserFormView.
+    ## path('form/', CustomUserFormView.as_view(), name='customuser_form'),
+
+    # path('login/', loginuser, name='loginuser'),
+    # path('logout/', logoutuser, name='logoutuser'),
+    # path('signup/', signupuser, name='signupuser'),
 ]
